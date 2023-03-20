41efe42edb6242778a24bbba335e5f6f07e4227f
Tanvir-Alam625 <mdtanviralamht731@gmail.com>
2023-03-20 12:28:57 +0600

    Add Header compo into signIn & SignUp page

---
+++ src/components/auth/Header.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Logo from "../../images/paper.png";
+function Header() {
+  return (
+    <div className="max-w-[1100px] my-7 mx-auto flex justify-between px-1 lg:px-0">
+      {/* Logo */}
+      <div className="flex gap-2">
+        <img src={Logo} className="h-[44px] w-[50px] " alt="logo-img" />
+        <h2 className="text-slate-800 font-bold text-3xl">Stack</h2>
+      </div>
+      {/* Languages  */}
+      <div className="text-slate-400 bg-slate-100 p-4 rounded-xl border-2 border-slate-100 focus-within:border-slate-400">
+        <select
+          name="languages"
+          className="outline-none focus:outline-none bg-transparent h-full"
+        >
+          <option className=" bg-slate-100" value="english(UK)">
+            English (UK)
+          </option>
+          <option className=" bg-slate-100" value="english(USA)">
+            English (USA)
+          </option>
+          <option className=" bg-slate-100" value="spanish">
+            Spanish
+          </option>
+          <option className=" bg-slate-100" value="Hindi">
+            Hindi
+          </option>
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -3,140 +3,146 @@ import GoogleIcon from "../../images/googleIcon.png";
 import AppleIcon from "../../images/appleIcon.png";
 import { FiAtSign, FiMeh, FiLock, FiEye, FiEyeOff } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import Header from "./Header";
 
 function Signup() {
   const [errorMessages, setErrorMessages] = useState({});
   const [passwordHideShow, setPasswordHideShow] = useState(false);
   return (
-    <div className="min-h-screen w-full flex flex-col items-center justify-center ">
-      <div className="max-w-[32rem] px-auto">
-        {/* =============Page Header Start ========= */}
-        <h1 className="text-2x font-bold text-slate-800 text-center my-5">
-          Getting Started
-        </h1>
-        <div className="flex flex-col gap-8">
-          <p className="text-slate-400 text-sm font-medium text-center">
-            Create an account to continue!
-          </p>
-          <div className="flex flex-col md:flex-row gap-6 lg:gap-8">
-            <button className=" transition duration-150 ease-in-out hover:bg-slate-100 bg-[#F0F5FA] rounded-xl text-slate-400 flex gap-2 py-5 px-[30px] ">
-              <img src={GoogleIcon} className="h-6" alt="google-img" />
-              <span className="text-sm font-medium">Sign Up with Google</span>
-            </button>
-            <button className=" transition duration-150 ease-in-out hover:bg-slate-100 bg-[#F0F5FA] rounded-xl text-slate-400 flex gap-2 py-5 px-[30px] ">
-              <img src={AppleIcon} className="h-6" alt="apple-img" />
-              <span className="text-sm font-medium">Sign Up with Apple ID</span>
-            </button>
-          </div>
-        </div>
-        {/* =============Page Header End ========= */}
-        {/* =============Divider Stat  ========= */}
-        <div className="divider-container w-full my-7  relative flex justify-center items-center">
-          <div className="w-full border-b-2 border-slate-200 h-1"></div>
-          <div className="absolute  -top-2 left-[50%] text-slate-400 px-4 bg-white translate-x-[-50%]">
-            OR
-          </div>
-        </div>
-        {/* =============Divider End ========= */}
-        <form className="my-7">
-          {/* Email  */}
-          <div className="my-6">
-            <div className="border-2 flex border-slate-200  focus-within:border-slate-400 rounded-xl ">
-              <div className="pt-5 pb-5 pl-5 pr-3 rounded-l-xl">
-                <FiAtSign className="text-slate-500 h-6 font-medium" />
-              </div>
-              <input
-                className="w-full rounded-r-xl px-2 font-medium text-sm outline-none focus:outline-none"
-                type="text"
-                placeholder="Your Email"
-                autoComplete="off"
-              />
-            </div>
-            {errorMessages?.email ? (
-              <p className="text-red-500 text-sm font-medium my-4"></p>
-            ) : null}
-          </div>
-          {/* Name  */}
-          <div className="my-6">
-            <div className="border-2 flex border-slate-200  focus-within:border-slate-400 rounded-xl ">
-              <div className="pt-5 pb-5 pl-5 pr-3 rounded-l-xl">
-                <FiMeh className="text-slate-500 h-6 font-medium" />
-              </div>
-              <input
-                className="w-full rounded-r-xl px-2 font-medium text-sm outline-none focus:outline-none"
-                type="text"
-                placeholder="Your Name"
-                autoComplete="off"
-              />
-            </div>
-            {errorMessages?.name ? (
-              <p className="text-red-500 text-sm font-medium my-4"></p>
-            ) : null}
-          </div>
-          {/* Password  */}
-          <div className="my-6">
-            <div className="border-2 flex relative  border-slate-200  focus-within:border-slate-400 rounded-xl ">
-              <div className="pt-5 pb-5 pl-5 pr-3 rounded-l-xl">
-                <FiLock className="text-slate-500 h-6 font-medium" />
-              </div>
-              <input
-                className="w-full rounded-r-xl px-2 font-medium text-sm outline-none focus:outline-none"
-                type={passwordHideShow ? "text" : "password"}
-                placeholder="Create Password"
-                autoComplete="off"
-              />
-              <div
-                onClick={() => setPasswordHideShow(!passwordHideShow)}
-                className="p-2 absolute  right-3 top-1/2 cursor-pointer translate-y-[-50%] z-50"
-              >
-                {passwordHideShow ? (
-                  <FiEye className="text-slate-500  h-6 font-medium" />
-                ) : (
-                  <FiEyeOff className="text-slate-500  h-6 font-medium" />
-                )}
-              </div>
-            </div>
-            {errorMessages?.name ? (
-              <p className="text-red-500 text-sm font-medium my-4"></p>
-            ) : null}
-          </div>
-          {/* ProgressBar  */}
-          <div className=" grid grid-cols-6 gap-2">
-            <div className="bg-green-500 rounded-xl p-1"></div>
-            <div className="bg-green-500 rounded-xl p-1"></div>
-            <div className="bg-green-500 rounded-xl p-1"></div>
-            <div className="bg-green-500 rounded-xl p-1"></div>
-            <div className="bg-slate-300 rounded-xl p-1"></div>
-            <div className="bg-slate-300 rounded-xl p-1"></div>
-          </div>
-          {/* Terms & Condition  */}
-          <div className="my-6 flex gap-4 items-center">
-            <input
-              type="checkbox"
-              id="terms"
-              className="h-4 w-4 rounded-xl bg-slate-300 border !border-slate-400 outline-none  focus:outline-none"
-            />
-            <label htmlFor="terms" className="text-sm text-slate-400">
-              I agree to the Terms & Conditions
-            </label>
-          </div>
-          {/* Submit Button  */}
-          <div className="my-6">
-            <button className="w-full bg-blue-500 p-5 rounded-xl cursor-pointer duration-300 ease-in-out transition-all text-slate-50 font-medium text-sm hover:bg-blue-600">
-              Sign Up
-            </button>
-          </div>
-          <div className="flex justify-center">
-            <p className="text-sm font-medium text-slate-400">
-              Already have an account?
-              <Link to="/signIn" className="text-blue-500">
-                Sign In
-              </Link>
+    <>
+      <Header />
+      <div className="min-h-screen flex items-center">
+        <div className="max-w-[32rem] mx-auto">
+          {/* =============Page Header Start ========= */}
+          <h1 className="text-2x font-bold text-slate-800 text-center my-5">
+            Getting Started
+          </h1>
+          <div className="flex flex-col gap-8">
+            <p className="text-slate-400 text-sm font-medium text-center">
+              Create an account to continue!
             </p>
+            <div className="flex flex-col md:flex-row gap-6 lg:gap-8">
+              <button className=" transition duration-150 ease-in-out hover:bg-slate-100 bg-[#F0F5FA] rounded-xl text-slate-400 flex gap-2 py-5 px-[30px] ">
+                <img src={GoogleIcon} className="h-6" alt="google-img" />
+                <span className="text-sm font-medium">Sign Up with Google</span>
+              </button>
+              <button className=" transition duration-150 ease-in-out hover:bg-slate-100 bg-[#F0F5FA] rounded-xl text-slate-400 flex gap-2 py-5 px-[30px] ">
+                <img src={AppleIcon} className="h-6" alt="apple-img" />
+                <span className="text-sm font-medium">
+                  Sign Up with Apple ID
+                </span>
+              </button>
+            </div>
           </div>
-        </form>
+          {/* =============Page Header End ========= */}
+          {/* =============Divider Stat  ========= */}
+          <div className="divider-container w-full my-7  relative flex justify-center items-center">
+            <div className="w-full border-b-2 border-slate-200 h-1"></div>
+            <div className="absolute  -top-2 left-[50%] text-slate-400 px-4 bg-white translate-x-[-50%]">
+              OR
+            </div>
+          </div>
+          {/* =============Divider End ========= */}
+          <form className="my-7">
+            {/* Email  */}
+            <div className="my-6">
+              <div className="border-2 flex border-slate-200  focus-within:border-slate-400 rounded-xl ">
+                <div className="pt-5 pb-5 pl-5 pr-3 rounded-l-xl">
+                  <FiAtSign className="text-slate-500 h-6 font-medium" />
+                </div>
+                <input
+                  className="w-full rounded-r-xl px-2 font-medium text-sm outline-none focus:outline-none"
+                  type="text"
+                  placeholder="Your Email"
+                  autoComplete="off"
+                />
+              </div>
+              {errorMessages?.email ? (
+                <p className="text-red-500 text-sm font-medium my-4"></p>
+              ) : null}
+            </div>
+            {/* Name  */}
+            <div className="my-6">
+              <div className="border-2 flex border-slate-200  focus-within:border-slate-400 rounded-xl ">
+                <div className="pt-5 pb-5 pl-5 pr-3 rounded-l-xl">
+                  <FiMeh className="text-slate-500 h-6 font-medium" />
+                </div>
+                <input
+                  className="w-full rounded-r-xl px-2 font-medium text-sm outline-none focus:outline-none"
+                  type="text"
+                  placeholder="Your Name"
+                  autoComplete="off"
+                />
+              </div>
+              {errorMessages?.name ? (
+                <p className="text-red-500 text-sm font-medium my-4"></p>
+              ) : null}
+            </div>
+            {/* Password  */}
+            <div className="my-6">
+              <div className="border-2 flex relative  border-slate-200  focus-within:border-slate-400 rounded-xl ">
+                <div className="pt-5 pb-5 pl-5 pr-3 rounded-l-xl">
+                  <FiLock className="text-slate-500 h-6 font-medium" />
+                </div>
+                <input
+                  className="w-full rounded-r-xl px-2 font-medium text-sm outline-none focus:outline-none"
+                  type={passwordHideShow ? "text" : "password"}
+                  placeholder="Create Password"
+                  autoComplete="off"
+                />
+                <div
+                  onClick={() => setPasswordHideShow(!passwordHideShow)}
+                  className="p-2 absolute  right-3 top-1/2 cursor-pointer translate-y-[-50%] z-50"
+                >
+                  {passwordHideShow ? (
+                    <FiEye className="text-slate-500  h-6 font-medium" />
+                  ) : (
+                    <FiEyeOff className="text-slate-500  h-6 font-medium" />
+                  )}
+                </div>
+              </div>
+              {errorMessages?.name ? (
+                <p className="text-red-500 text-sm font-medium my-4"></p>
+              ) : null}
+            </div>
+            {/* ProgressBar  */}
+            <div className=" grid grid-cols-6 gap-2">
+              <div className="bg-green-500 rounded-xl p-1"></div>
+              <div className="bg-green-500 rounded-xl p-1"></div>
+              <div className="bg-green-500 rounded-xl p-1"></div>
+              <div className="bg-green-500 rounded-xl p-1"></div>
+              <div className="bg-slate-300 rounded-xl p-1"></div>
+              <div className="bg-slate-300 rounded-xl p-1"></div>
+            </div>
+            {/* Terms & Condition  */}
+            <div className="my-6 flex gap-4 items-center">
+              <input
+                type="checkbox"
+                id="terms"
+                className="h-4 w-4 rounded-xl bg-slate-300 border !border-slate-400 outline-none  focus:outline-none"
+              />
+              <label htmlFor="terms" className="text-sm text-slate-400">
+                I agree to the Terms & Conditions
+              </label>
+            </div>
+            {/* Submit Button  */}
+            <div className="my-6">
+              <button className="w-full bg-blue-500 p-5 rounded-xl cursor-pointer duration-300 ease-in-out transition-all text-slate-50 font-medium text-sm hover:bg-blue-600">
+                Sign Up
+              </button>
+            </div>
+            <div className="flex justify-center">
+              <p className="text-sm font-medium text-slate-400">
+                Already have an account?
+                <Link to="/signIn" className="text-blue-500">
+                  Sign In
+                </Link>
+              </p>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
